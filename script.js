7ae4742a623e1c3b4363692f323a90fa3228ec0c
Malvina <malvinapalmero.dev@gmail.com>
2024-12-02 18:42:43 -0300

    Actualización CSS y HTML

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                    observer.unobserve(entry.target); // Deja de observar una vez animada
+                }
+            });
+        },
+        {
+            threshold: 0.6, // Activa cuando el 20% del elemento es visible
+        }
+    );
+
+    const elementosAnimados = document.querySelectorAll(".animado");
+    elementosAnimados.forEach((elemento) => observer.observe(elemento));
+});
